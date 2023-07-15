74d466563cca54c1cceff17a62beb6f9b29da5b2
Aparna Samal <samalaparna9186@gmail.com>
2023-07-16 00:23:02 +0530

    Time: 15 ms (26.00%), Space: 47.1 MB (71.92%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            if(!hs.containsKey(nums[i])){
+                hs.put(nums[i],1);
+            }
+            else{
+                hs.put(nums[i],hs.get(nums[i])+1);
+            }
+        }
+        int count=nums.length/2;
+        for(int i=0; i < nums.length; i++){
+            if(hs.get(nums[i]) > count){
+                return nums[i];
+            }
+        }
+        return -1;    
+    }
+}
